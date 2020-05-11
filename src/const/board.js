679cd9f7f0cf7a6ff088eda6ccb0efb8d5d04b38
Josef Jadrny <bc.josefjadrny@gmail.com>
2020-05-11 11:21:35 +0200

    chore: AI logic enum rollback

--- src/const/board.js
+++ src/const/board.js
@@ -5,9 +5,9 @@ export const COLORS = {
     WHITE: 'white'
 }
 export const COMPUTER_LEVELS = {
-    "Monkey": 1,
-    "Beginner": 2,
-    "Advanced": 3,
+    "Monkey": 0,
+    "Beginner": 1,
+    "Advanced": 2,
 }
 export const SETTINGS = {
     computerLevel: COMPUTER_LEVELS.Advanced,
